b73a8493464eb6360fddff10bc67074a2dee68b2
ncherevichkin <ncherevichkin@mail.ru>
2019-02-11 17:00:11 +0700

    2.1

--- 1.2 laba - oop/shapes.cpp
+++ 1.2 laba - oop/shapes.cpp
@@ -3,6 +3,7 @@
 // геометрических фигурах
 #include "box_atd.h"
 #include "share_atd.h"
+#include "tetraidr_atd.h"
 using namespace std;
 #include "shape_atd.h"
 
@@ -19,6 +20,9 @@ namespace simple_shapes {
 			break;
 		case 2:
 			sp = new share;
+			break;
+		case 3:
+			sp = new tetraidr;
 			break;
 		default:
 			return 0;
